cac5b37d592f79229df0bf19a405b7777b5ab9fe
Ana Paula <apaulavsz@gmail.com>
2025-10-19 15:46:27 -0600

    Descripción del cambio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { loadRemoteModule } from '@angular-architects/module-federation';
+export const routes: Routes = [{
+path: 'micro1'
+, loadComponent: () =>
+loadRemoteModule({
+type: 'module'
+,
+remoteEntry: 'http://localhost:4201/remoteEntry.js',
+exposedModule: './Component'
+}).then(m => m.AppComponent)
+,
+}];
